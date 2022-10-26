347b33f3c4384dafe3633d26cfb78271a94d23f4
Michaël Dierick <michael.dierick@redpencil.io>
2022-10-26 15:13:48 +0200

    remove bugged registring functionality

--- app/controllers/register.ts
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-import { action } from '@ember/object';
-
-export default class RegisterController extends Controller {
-  @action
-  onRegisterFailed(error: string) {
-    console.log(error);
-  }
-}
--- app/routes/register.ts
+++
@@ -1,3 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class RegisterRoute extends Route {}
