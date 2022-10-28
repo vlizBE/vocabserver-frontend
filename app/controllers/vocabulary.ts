a7417ef918feebc4c1f91e4a42c86e67dfb0c63a
Michaël Dierick <michael.dierick@redpencil.io>
2022-10-28 12:28:31 +0200

    allow frontend triggering of unification job

---
+++ app/controllers/vocabulary.ts
@@ -0,0 +1,27 @@
+import Model from '@ember-data/model';
+import Store from '@ember-data/store';
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { task } from 'ember-concurrency';
+
+export default class VocabularyController extends Controller {
+  @service declare store: Store;
+
+  @action
+  async unifyVocab(id: string) {
+    await fetch(`/content-unification-jobs/${id}/run`, {
+      method: 'POST',
+    });
+  }
+
+  @task
+  *createAndRunDownloadJob(fileUri: string) {
+    const record = this.store.createRecord('content-unification-job', {
+      created: new Date(),
+      sources: fileUri,
+    });
+    yield record.save();
+    yield this.unifyVocab(record.id);
+  }
+}
