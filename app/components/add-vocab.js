96d6b7be16b6214f380c45c653509c14f58abf48
Kobe Mertens <kobe.mertens@hotmail.com>
2022-11-23 15:35:28 +0100

    added radio button for vocab format

--- app/components/add-vocab.js
+++ app/components/add-vocab.js
@@ -9,12 +9,20 @@ import { service } from '@ember/service';
 export default class AddVocabComponent extends Component {
   @service store;
 
+  formatOptions = [
+    { label: 'N3', value: 'http://www.w3.org/ns/formats/N3' },
+    { label: 'N-Triples', value: 'http://www.w3.org/ns/formats/N-Triples' },
+    { label: 'RDF_XML', value: 'http://www.w3.org/ns/formats/RDF_XML' },
+    { label: 'RDFa', value: 'http://www.w3.org/ns/formats/RDFa' },
+    { label: 'Turtle', value: 'http://www.w3.org/ns/formats/Turtle' },
+  ];
+
   @action
   async submit(params) {
     const vocabularyMeta = this.store.createRecord('vocabulary', params);
     const sourceDataset = this.store.createRecord('dataset', params);
     sourceDataset.downloadPage = params.downloadPage;
-    sourceDataset.format = params.format;
+    sourceDataset.format = params.format.value;
     sourceDataset.sparqlEndpoint = params.sparqlEndpoint;
 
     vocabularyMeta.sourceDataset = sourceDataset;
