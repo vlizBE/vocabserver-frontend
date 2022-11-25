73f10a4213c885c39f4a4846e34968bee3176272
Michaël Dierick <michael.dierick@redpencil.io>
2022-11-25 15:07:39 +0100

    fix status check for unification step

--- app/routes/vocabulary-mapping-wizard.js
+++ app/routes/vocabulary-mapping-wizard.js
@@ -8,7 +8,7 @@ export default class VocabulariesRoute extends Route {
   async model(params) {
     const ds = await this.store.query('dataset', {
       'filter[vocabulary][:id:]': params.vocabulary_id,
-      include: 'data-dumps,vocabulary,vocabulary.mapping-shape,vocabulary.mapping-shape.property-shapes,classes,properties',
+      include: 'data-dumps,vocabulary,vocabulary.mapping-shape,vocabulary.mapping-shape.property-shapes',
     });
     if (ds.length) {
       return ds.firstObject;
@@ -17,11 +17,18 @@ export default class VocabulariesRoute extends Route {
 
   async afterModel(model) {
     this.vocabulary = await model.vocabulary;
+    this.lastUnification = (await this.store.query('content-unification-job', {
+      'filter[sources]': this.vocabulary.uri,
+      'filter[status]': 'http://vocab.deri.ie/cogs#Success',
+      'page[size]': 1,
+      sort: '-created',
+    })).firstObject;
   }
 
   setupController(controller) {
     super.setupController(...arguments);
     controller.vocabulary = this.vocabulary;
+    controller.lastUnification = this.lastUnification;
   }
 
   @action
