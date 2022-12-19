2368e7fd342645c6b5bfb385e8ad868ec3857a9f
Kobe Mertens <kobe.mertens@hotmail.com>
2022-12-19 13:29:16 +0100

    basic title edit

---
+++ app/components/inline-edit.js
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class InlineEdit extends Component {
+  @tracked isEditing = false;
+
+  constructor() {
+    super(...arguments);
+    this.value = this.args.value;
+  }
+
+  @action
+  startEdit() {
+    this.isEditing = true;
+  }
+
+  @action
+  async submit() {
+    this.isEditing = false;
+    this.args.onSubmit(this.value);
+  }
+
+  @action
+  cancel() {
+    this.isEditing = false;
+  }
+}
