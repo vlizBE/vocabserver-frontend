76d8fb1e08eb18f5ccef1b9398060ac3703167d9
Leander Lismond <36822732+l5r@users.noreply.github.com>
2025-12-09 15:22:17 +0100

    Move vocab delete to background task
    Deleting a vocab now happens in the background, and the vocab list UI
    shows the status of the delete task.

--- app/models/vocabulary.js
+++ app/models/vocabulary.js
@@ -8,4 +8,22 @@ export default class VocabularyModel extends Model {
 
   @hasMany('dataset') sourceDatasets;
   @belongsTo('shacl-node-shape') mappingShape;
+  @hasMany('data-container') dataContainers;
+
+  get deleting() {
+    return this.deletingTask
+  }
+  get deletingTask() {
+    return this._getDeletingTask()
+  }
+
+  async _getDeletingTask() {
+    const dataContainers = await this.dataContainers
+    const tasks = await Promise.all(dataContainers.map((dc) => dc.inputFromTasks))
+
+    return tasks.flat().find((task) => {
+      console.log(task.operation)
+      return task.operation === 'http://mu.semte.ch/vocabularies/ext/VocabDeleteJob'
+    })
+  }
 }
