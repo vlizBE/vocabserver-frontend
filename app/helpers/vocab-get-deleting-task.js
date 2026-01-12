aa43e99a1c30714220395b3439312de0018a757b
Ruben <ruben.de.facq@redpencil.io>
2026-01-12 16:17:51 +0100

    fix some linting for new files
    Fix some basic linting but only for newer files

--- app/helpers/vocab-get-deleting-task.js
+++ app/helpers/vocab-get-deleting-task.js
@@ -1,10 +1,9 @@
-import GetTaskByInputAndOperation from "./get-task-by-input-and-operation";
+import GetTaskByInputAndOperation from './get-task-by-input-and-operation';
 
 export default class VocabGetDeletingTask extends GetTaskByInputAndOperation {
-
   compute([vocab]) {
     return super.compute([vocab], {
-      operation: 'http://mu.semte.ch/vocabularies/ext/VocabDeleteJob'
-    })
+      operation: 'http://mu.semte.ch/vocabularies/ext/VocabDeleteJob',
+    });
   }
 }
